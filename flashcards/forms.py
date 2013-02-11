56560f9cff29f1111192270006eb18b80a4828b2
Alex <ragsdale.a@gmail.com>
2013-02-11 15:56:15 -0600

    Basic forms.

--- flashcards/forms.py
+++ flashcards/forms.py
@@ -1,1 +1,26 @@
+from datetime import datetime
+
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
 from flashcards.models import Set, Card
+
+
+class SetForm(TendenciBaseForm):
+
+    class Meta:
+        model = Set
+        fields = (
+            'title',
+            'level',
+        )
+
+class CardForm(TendenciBaseForm):
+
+    class Meta:
+        model = Card
+        fields = (
+            'question',
+            'answer',
+            'set',
+        )
